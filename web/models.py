82193eeb716c2637d7ae36c4bf7414b80c667899
Navid falahatchian <navidfalahatchian@Navids-MacBook-Pro.local>
2024-03-21 23:05:43 +0330

    standard version 1

--- web/models.py
+++ web/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import date
+
+
+
+# 
+
+Gender = (
+    (0, 'Male'),
+    (1, 'Female'),
+    (2, 'Not specified'),
+)
+
+HeightUnits = (
+    (0, 'Cm'),
+    (1, 'Inch')
+)
+
+WeightUnits = (
+    (0, 'KG'),
+    (1, 'Pound')
+)
+
 
 # Create your models here.
+
+
+class MainUser(AbstractUser):
+    
+    FirstName = models.CharField(max_length=128)
+    LastName = models.CharField(max_length=128)
+    Email = models.EmailField(max_length=70, blank=True, unique=True)
+    PhoneNumber = models.CharField(max_length=128)
+    gender = models.IntegerField(choices=Gender)
+    UserHeight = models.SmallIntegerField()
+    UserWeight = models.SmallIntegerField()
+    BirthDate = models.DateField(null=True, blank=True)
+    
+
+
+    def calculate_age(self):
+        today = date.today()
+        if self.BirthDate:
+            return today.year - self.BirthDate.year
+        return None
+    
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='main_users_groups'  # Custom related_name for the groups field
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='main_users_permissions'  # Custom related_name for the user_permissions field
+    )
+
+    def __str__(self):
+        return self.username
+    
+
+
+class Exercise(models.Model):
+    user = models.ForeignKey(MainUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+
+# Signal handler function to create exercises for the authenticated user
+
